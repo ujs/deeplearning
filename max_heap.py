1e4c75a05ccc93bdf5840c6a93bfef2b945de0fe
ujala <ujalashanker@gmail.com>
2019-04-20 16:25:58 -0400

    changes

--- max_heap.py
+++ max_heap.py
@@ -1,4 +1,5 @@
 def max_heapify(A,i):
+    '''to hangle single violation only'''
     l = 2*(i+1)-1
     r = 2*(i+1)-1
     if l <= len(A) and A[l]>A[i]:
@@ -8,22 +9,32 @@ def max_heapify(A,i):
         largest = r
     if largest != i:
         A[i] = A[largest]
-        max_heapify(A,largest)
-
-
+        retrun max_heapify(A,largest)
 
 
 
 def build_maxheap(A):
+    '''to product max heap from unordered array'''
     for i in range (len(A//2-1,0,-1)):
-        max_heapify(A,i)
-
-
+        ans = max_heapify(A,i)
+        return ans
 
 
 def heap_sort(A):
+    '''to sort a max heap'''
+    ans = []
+    build_maxheap(A)
+    k = len(A)
+    for i in range (k-1,0,-1):  #to swap the first and last element of the max heap
+        swap = A[i]
+        A[i] = A[0]
+        A[0] = swap
+        ans.append(A[i])
+
+        max_heapify(A,0)
+        return ans
 
 
 
 
-def maxheap_insert(A,k):
+#def maxheap_insert(A,k):
