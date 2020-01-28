58662ed46885e78dafaada8ce980b1e7f4970003
ujala <ujalashanker@gmail.com>
2020-01-28 10:21:20 -0500

    conv2d

--- neural_nets/5_dl.py
+++ neural_nets/5_dl.py
@@ -8,3 +8,8 @@ model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(64,(3,3),activation = 'relu'))
 model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(64,(3,3),activation = 'relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation = 'relu'))
+model.add(layers.Dense(10, activation = 'softmax'))
+
+model.summary()
