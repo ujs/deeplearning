3b60e71fdd02d5fbc0b1848e354a329779073f29
ujala <ujalashanker@gmail.com>
2020-01-29 09:24:54 -0500

    conv2d

--- neural_nets/5_dl.py
+++ neural_nets/5_dl.py
@@ -45,3 +45,12 @@ test_loss, test_accuracy = model.evaluate(test_images, test_labels)
 ##################################################################################################################################
 
 #Cat and Dog data CNN
+ #Data Generators- used to draw random samples without replacements of image data and convert them to tensors
+
+ from keras.preprocessing.image import ImageDataGenerator
+
+ train_datagen = ImageDataGenerator(rescale = 1./255)
+ validation_datagen = ImageDataGenerator(rescale = 1./255)
+
+ train_generator = train_datagen.flow_from_directory(train_dir,target_size= (150,150), batch_size = 20, class_mode = 'binary')
+ validation_generator =
