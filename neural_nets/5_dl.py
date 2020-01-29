9a2afe6ca9de32371efb747f4706062f4875af43
ujala <ujalashanker@gmail.com>
2020-01-29 09:26:18 -0500

    conv2d

--- neural_nets/5_dl.py
+++ neural_nets/5_dl.py
@@ -53,4 +53,4 @@ test_loss, test_accuracy = model.evaluate(test_images, test_labels)
  validation_datagen = ImageDataGenerator(rescale = 1./255)
 
  train_generator = train_datagen.flow_from_directory(train_dir,target_size= (150,150), batch_size = 20, class_mode = 'binary')
- validation_generator =
+ validation_generator = validation_datagen.flow_from_directory(validation_dir, target_size = (150,150), batch_size = 20, class_mode = 'binary')
