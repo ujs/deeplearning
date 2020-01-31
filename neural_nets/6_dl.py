789ad43652ccea6c87d2c5efefdf7a5683bbd50f
ujala <ujalashanker@gmail.com>
2020-01-31 10:44:20 -0500

    text one hot encoding

--- neural_nets/6_dl.py
+++ neural_nets/6_dl.py
@@ -1,14 +1,33 @@
 #Word level one hot encoding
 
 import numpy as np
+import string
 
-X=  'the cat sat on the mat'
+samples =  ['the cat went for a walk', 'the dog ate my burger']
 
-def one_hot_encoding(X, spa):
 
-    encoded = np.zeros((len(X),6))
 
-    dimensionality  = 1000
+max_len = 10
+
+def tokenization(samples):
+    token_dict = {}
+    for sample in samples:
+        for value, key in enumerate(samples.split(' ')):
+            if key not in token_dict:
+            token_dict[key] = value
+    dimensionality = len(token_dict)
+
+    return token_dict, dimensionality
+
+
+
+
+
+def one_hot_encoding(samples, dimensionality, max_len):
+
+    encoded = np.zeros((len(samples),max_len, dimensionality))
+
+
 
     dict = {}
 
