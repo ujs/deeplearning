6eb86f1b134d813ebf9869d60cc9dc920efa6796
ujala <ujalashanker@gmail.com>
2020-02-10 21:43:14 -0500

    mandelbrot set

--- neural_nets/manderbolt.py
+++ neural_nets/manderbolt.py
@@ -1,4 +1,10 @@
 # x refers to real part , y refers to imaginary part
 
-def mandelbrot(z, iterations, horizon = 2.0):
-    
+def mandelbrot(z, iterations, horizon = 2.0, xmin, xmax, ymin, ymax,xn,yn):
+
+    real = np.linspace(xmin,xmax,xn, dtype = np.float32)
+    imaginary = np.linspace(ymin,ymax,yn, dtype = np.float32)
+
+    C= real +imaginary[:,None] * 1j
+
+    Z = np.zeros(C.shape, dtype = complex)
