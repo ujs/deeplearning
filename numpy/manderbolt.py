4aa4e44c6bbfb3bcd030716430292310abc517a3
ujala <ujalashanker@gmail.com>
2020-02-13 16:00:57 -0500

    rnn

--- numpy/manderbolt.py
+++ numpy/manderbolt.py
@@ -8,8 +8,6 @@ def mandelbrot(iterations,xmin, xmax, ymin, ymax,xn,yn, horizon = 2.0):
     C= real +imaginary[:,None] * 1j
 
     N = np.zeros(C.shape,np.int64)
-
-
     Z = np.zeros(C.shape, np.complex64)
 
     for n in range(iterations):
@@ -21,3 +19,35 @@ def mandelbrot(iterations,xmin, xmax, ymin, ymax,xn,yn, horizon = 2.0):
 
 
     return Z.shape, N.shape
+
+
+
+def mandelbrot_numpy_2(xmin, xmax, ymin, ymax, xn, yn, itermax, horizon=2.0):
+    Xi, Yi = np.mgrid[0:xn, 0:yn]
+    Xi, Yi = Xi.astype(np.uint32), Yi.astype(np.uint32)
+    X = np.linspace(xmin, xmax, xn, dtype=np.float32)[Xi]
+    Y = np.linspace(ymin, ymax, yn, dtype=np.float32)[Yi]
+    C = X + Y*1j
+    N_ = np.zeros(C.shape, dtype=np.uint32)
+    Z_ = np.zeros(C.shape, dtype=np.complex64)
+    Xi.shape = Yi.shape = C.shape = xn*yn
+
+    Z = np.zeros(C.shape, np.complex64)
+    for i in range(itermax):
+        if not len(Z): break
+
+        # Compute for relevant points only
+        np.multiply(Z, Z, Z)
+        np.add(Z, C, Z)
+
+        # Failed convergence
+        I = abs(Z) > horizon
+        N_[Xi[I], Yi[I]] = i+1
+        Z_[Xi[I], Yi[I]] = Z[I]
+
+        # Keep going with those who have not diverged yet
+        np.negative(I,I)
+        Z = Z[I]
+        Xi, Yi = Xi[I], Yi[I]
+        C = C[I]
+    return Z_.T, N_.T
