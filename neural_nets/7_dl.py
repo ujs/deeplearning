856a2922ab96265a58cc700db52521566fa0ecf7
ujala <ujalashanker@gmail.com>
2020-02-14 14:43:32 -0500

    keras - layers as functions

--- neural_nets/7_dl.py
+++ neural_nets/7_dl.py
@@ -1,8 +1,11 @@
 from keras import Input, layers
-input_tensor = Input(shape= (32,))
+input_tensor = Input(shape= (64,))
 
 
 #instead of using sequential linear layers (.add), use a variable to save layer as a function
 
-dense = layers.Dense(32,activation ='relu')
-output_tensor = dense(input_tensor)   #returns model output
+dense_1 = layers.Dense(32,activation ='relu')
+dense_2 = layers.Dense(32, activation ='relu')
+dense_3 = layers.Dense(10, activation ='softmax')
+
+output_tensor(dense_3(dense_2(dense_1(input_tensor))))
