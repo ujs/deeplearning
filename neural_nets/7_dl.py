8fa2e8f036e0626aeae64d187acfd71c02c2ecf4
ujala <ujalashanker@gmail.com>
2020-02-14 14:37:58 -0500

    keras - layers as functions

--- neural_nets/7_dl.py
+++ neural_nets/7_dl.py
@@ -3,3 +3,6 @@ input_tensor = Input(shape= (32,))
 
 
 #instead of using sequential linear layers (.add), use a variable to save layer as a function
+
+dense = layers.Dense(32,activation ='relu')
+output_tensor = dense(input_tensor)   #returns model output
