e8af6c0efea12895b49275d64973ba14c07abf5d
ujala <ujalashanker@gmail.com>
2020-02-15 12:04:46 -0500

    keras - layers as functions

--- neural_nets/7_dl.py
+++ neural_nets/7_dl.py
@@ -9,3 +9,17 @@ x = layers.Dense(32, activation ='relu')(x)
 output_tensor = layers.Dense(10, activation ='softmax')(x)
 
 model = Model(input_tensor, output_tensor)  #instatiating a model. like magic- behind the scenes, keras retrives the flow from input to output via layers
+
+
+from keras import layers
+branch_a = layers.Conv2D(128, 1,
+activation='relu', strides=2)(x)
+branch_b = layers.Conv2D(128, 1, activation='relu')(x)
+branch_b = layers.Conv2D(128, 3, activation='relu', strides=2)(branch_b)
+branch_c = layers.AveragePooling2D(3, strides=2)(x)
+branch_c = layers.Conv2D(128, 3, activation='relu')(branch_c)
+branch_d = layers.Conv2D(128, 1, activation='relu')(x)
+branch_d = layers.Conv2D(128, 3, activation='relu')(branch_d)
+branch_d = layers.Conv2D(128, 3, activation='relu', strides=2)(branch_d)
+output = layers.concatenate(
+[branch_a, branch_b, branch_c, branch_d], axis=-1)
